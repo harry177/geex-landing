1ff04569474510c94cf0c1869dcc3230c22713bb
Artem Prygunov <harry177@rambler.ru>
2024-12-18 18:30:48 +0100

    feat: complete video section

---
+++ src/components/sections/PhotoSection/data.ts
@@ -0,0 +1,11 @@
+export const photoData = [
+  "photos/photo-1.jpg",
+  "photos/photo-2.jpg",
+  "photos/photo-3.jpg",
+  "photos/photo-4.jpg",
+  "photos/photo-5.jpg",
+  "photos/photo-6.jpg",
+  "photos/photo-7.jpg",
+  "photos/photo-8.jpg",
+  "photos/photo-9.jpg",
+];
